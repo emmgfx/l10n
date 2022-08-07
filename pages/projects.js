e257b186ce895246af735f3c41e91dcfd42912c0
Josep Viciana <emmgfx@gmail.com>
2022-08-07 22:20:55 +0200

    Projects heading

--- pages/projects.js
+++ pages/projects.js
@@ -12,20 +12,29 @@ const Projects = ({ projects }) => {
   const { currentModal, setCurrentModal } = useModal();
 
   return (
-    <div className="container mx-auto p-6">
-      <section className="">
-        <div className="mb-6 text-right">
-          <Button onClick={() => setCurrentModal(MODALS.NEW_PROJECT)}>
-            New Project
-          </Button>
-        </div>
-        <div className="grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-          {projects.map((project) => (
-            <ProjectItem key={project.id} project={project} />
-          ))}
-        </div>
-      </section>
-      {currentModal === MODALS.NEW_PROJECT && <NewProject />}
+    <div className="py-6 px-8">
+      <div className="container mx-auto">
+        <section className="">
+          <div className="flex items-center justify-between">
+            <h1 className="font-bold text-2xl text-gray-800">
+              Your current projects
+            </h1>
+            <Button
+              className="grow-0"
+              onClick={() => setCurrentModal(MODALS.NEW_PROJECT)}
+            >
+              New Project
+            </Button>
+          </div>
+          <div className="h-6" />
+          <div className="grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+            {projects.map((project) => (
+              <ProjectItem key={project.id} project={project} />
+            ))}
+          </div>
+        </section>
+        {currentModal === MODALS.NEW_PROJECT && <NewProject />}
+      </div>
     </div>
   );
 };
